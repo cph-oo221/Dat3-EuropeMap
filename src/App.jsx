e855e90776019e2ad0b343cae35c4592c6ab6b4a
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-11-16 09:43:03 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import EuropeMap from "./europeMap";
+
+function App() {
+  return (
+    <>
+      <EuropeMap />
+    </>
+  );
+}
+
+export default App;
---
+++ src/europeMap.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import SvgComponent from "./SvgComponent";
+
+function EuropeMap() {
+  const [contry, setContry] = useState("");
+  const [prevTarget, setPrevTarget] = useState(null);
+
+  useEffect(() => {
+    clearInterval();
+    console.log(contry);
+  }, [contry]);
+
+  function hanleClick(e) {
+    if (prevTarget) {
+      prevTarget.style.fill = "silver";
+    }
+
+    let id = e.target.id;
+
+    e.target.style.fill = "red";
+
+    if (id.includes("-")) {
+      id = id.split("-")[0];
+    }
+
+    console.log(id);
+    fetchCountries(id);
+
+    setPrevTarget(e.target);
+  }
+
+  const fetchCountries = (contryCode) => {
+    fetch(`https://restcountries.com/v3.1/alpha/${contryCode}`, {
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setContry(data))
+      .catch((error) => console.error("Error fetching joke:", error));
+  };
+
+  return (
+    <div onClick={hanleClick}>
+      <SvgComponent />
+    </div>
+  );
+}
+
+export default EuropeMap;
